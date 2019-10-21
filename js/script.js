0eb1015197a2322f976041d5e6790a741245ff98
Devvrat Shukla <devvratshukla@gmail.com>
2019-10-22 01:08:00 +0530

    feat: Module design pattern

--- js/script.js
+++ js/script.js
@@ -1,4 +1,56 @@
-var io = io || {};
+var chatModule = (function() {
+    let fromSelf = 'Me:',
+        fromComputer = 'PC:',
+        aSaid = ['This is a Cyber Chat'],
+        msgYes = "Yes, that's a great idea.",
+        msgNo = 'No, that must be a mistake.',
+        aSassyStuff = [
+            'Like mold on books, grow myths on history.',
+            'She moved like a poem and smiled like a sphinx.',
+            'As long as we don’t die, this is gonna be one hell of a story.',
+            'She laughed, and the desert sang.',
+            'You’ve got about as much charm as a dead slug.'
+        ];
+
+    function echo(msg) {
+        aSaid.push(`<div>${msg}</div>`);
+        const aSaidLength = aSaid.length;
+        const start = Math.max(aSaidLength - 6, 0);
+        let out = '';
+
+        for (let index = start; index < aSaidLength; index++) {
+            out += aSaid[index];
+        }
+
+        $('.advert').html(out);
+
+        $('#talk span').text(msg);
+    }
+
+    return {
+        talk(msg) {
+            echo(fromSelf + ' ' + msg);
+        },
+
+        replyYesOrNo() {
+            const msg = Math.random() > 0.5 ? msgYes : msgNo;
+            echo(fromComputer + ' ' + msg);
+        },
+
+        saySassyStuff() {
+            const msg = aSassyStuff[Math.floor(Math.random() * aSassyStuff.length)];
+            echo(fromSelf + ' ' + msg);
+        }
+    };
+})();
+
+$(document).ready(() => {
+    chatModule.talk('This is great');
+    chatModule.replyYesOrNo();
+    chatModule.saySassyStuff('Hey...');
+});
+
+/* var io = io || {};
 io.github = io.github || {};
 io.github.devvratshukla = io.github.devvratshukla || {};
 io.github.devvratshukla.jsdp = io.github.devvratshukla.jsdp || {};
@@ -46,3 +98,4 @@ io.github.devvratshukla.jsdp.chat = {
         $('#talk span').text(msg);
     }
 };
+ */
