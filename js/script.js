db00d049de85cd70566188645d38550a667983ea
Devvrat Shukla <devvratshukla@gmail.com>
2019-10-22 00:53:56 +0530

    feat: Object literal and namespace design pattern

--- js/script.js
+++ js/script.js
@@ -1,41 +1,48 @@
-let fromSelf = 'Me: ';
-let fromComputer = 'PC: ';
-let aSaid = ['This is a Cyber Chat'];
-let msgYes = "Yes, that's a great idea.";
-let msgNo = 'No, that must be a mistake.';
-let aSassyStuff = [
-    'Like mold on books, grow myths on history.',
-    'She moved like a poem and smiled like a sphinx.',
-    'As long as we don’t die, this is gonna be one hell of a story.',
-    'She laughed, and the desert sang.',
-    'You’ve got about as much charm as a dead slug.'
-];
+var io = io || {};
+io.github = io.github || {};
+io.github.devvratshukla = io.github.devvratshukla || {};
+io.github.devvratshukla.jsdp = io.github.devvratshukla.jsdp || {};
 
-function talk(msg) {
-    echo(fromSelf + ' ' + msg);
-}
+// namespace
+io.github.devvratshukla.jsdp.chat = {
+    fromSelf: 'Me: ',
+    fromComputer: 'PC: ',
+    aSaid: ['This is a Cyber Chat'],
+    msgYes: "Yes, that's a great idea.",
+    msgNo: 'No, that must be a mistake.',
+    aSassyStuff: [
+        'Like mold on books, grow myths on history.',
+        'She moved like a poem and smiled like a sphinx.',
+        'As long as we don’t die, this is gonna be one hell of a story.',
+        'She laughed, and the desert sang.',
+        'You’ve got about as much charm as a dead slug.'
+    ],
+    talk(msg) {
+        this.echo(this.fromSelf + ' ' + msg);
+    },
 
-function replayYesNo() {
-    const msg = Math.random() > 0.5 ? msgYes : msgNo;
-    echo(fromComputer + ' ' + msg);
-}
+    replayYesNo() {
+        const msg = Math.random() > 0.5 ? this.msgYes : this.msgNo;
+        this.echo(this.fromComputer + ' ' + msg);
+    },
 
-function saySassyStuff() {
-    const msg = aSassyStuff[Math.floor(Math.random() * aSassyStuff.length)];
-    echo(fromSelf + ' ' + msg);
-}
+    saySassyStuff() {
+        const msg = this.aSassyStuff[Math.floor(Math.random() * this.aSassyStuff.length)];
+        this.echo(this.fromSelf + ' ' + msg);
+    },
 
-function echo(msg) {
-    aSaid.push(`<div>${msg}</div>`);
-    const aSaidLength = aSaid.length;
-    const start = Math.max(aSaidLength - 6, 0);
-    let out = '';
+    echo(msg) {
+        this.aSaid.push(`<div>${this.fromSelf} ${msg}</div>`);
+        const aSaidLength = this.aSaid.length;
+        const start = Math.max(aSaidLength - 6, 0);
+        let out = '';
 
-    for (let index = start; index < aSaidLength; index++) {
-        out += aSaid[index];
+        for (let index = start; index < aSaidLength; index++) {
+            out += this.aSaid[index];
+        }
+
+        $('.advert').html(out);
+
+        $('#talk span').text(msg);
     }
-
-    $('.advert').html(out);
-
-    $('#talk span').text(msg);
-}
+};
